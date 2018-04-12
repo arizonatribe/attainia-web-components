d91aa78afcfab49c9f7f2939440b06e97a14e033
David Nunez <arizonatribe@gmail.com>
2018-04-11 22:21:37 -0700

    Merged in development and incrmented the version number

--- src/components/common/InputField.js
+++ src/components/common/InputField.js
@@ -4,25 +4,19 @@ import PropTypes from 'prop-types'
 import {either, pathOr, prop} from 'ramda'
 
 const BaseStyles = `
+    display: block;
     border-radius: 0;
+    width: 100%;
     box-sizing: border-box;
     padding: 5px;
+    font-size: 14px;
     line-height: 16px;
     height: 2.3em;
     box-shadow: none;
-    color: ${either(prop('color'), pathOr('darkgray', ['theme', 'colors', 'misc', 'gray', 'lightJet']))};
     &:focus {
         outline: none;
     }
 `
-const ExtraStyles = ({type}) => (
-    /(password|url|number|text|email)/i.test(type) ? `
-        height: 2.000em;
-        width: 100%;
-        font-size: 14px;
-        background-color: white;
-    ` : ''
-)
 const CheckboxStyles = ({type}) => (
     /(checkbox)/i.test(type) ? `
         visibility: hidden;
@@ -33,18 +27,20 @@ const CheckboxStyles = ({type}) => (
 )
 
 const TextArea = styled.textarea`
-    width: 100%;
+    ${BaseStyles}
     resize: none;
-    display: block;
     box-shadow: none;
     padding: ${pathOr('0.6em', ['padding'])};
+    color: ${either(prop('color'), pathOr('darkgray', ['theme', 'colors', 'misc', 'gray', 'lightJet']))};
+    background-color: ${either(prop('backgroundColor'), pathOr('white', ['theme', 'colors', 'grayscale', 'white']))};
     border: 1px solid ${either(prop('border'), pathOr('lightgray', ['theme', 'colors', 'misc', 'gray', 'timberwolf']))};
-    ${BaseStyles}
 `
 const Input = styled.input`
     ${BaseStyles}
-    ${ExtraStyles}
     ${CheckboxStyles}
+    color: ${either(prop('color'), pathOr('darkgray', ['theme', 'colors', 'misc', 'gray', 'lightJet']))};
+    background-color: ${either(prop('backgroundColor'), pathOr('white', ['theme', 'colors', 'grayscale', 'white']))};
+    border: 1px solid ${either(prop('border'), pathOr('lightgray', ['theme', 'colors', 'misc', 'gray', 'timberwolf']))};
 `
 const InputField = props => (
     /textarea/i.test(props.type) ?
