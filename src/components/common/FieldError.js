1f8f09d7db8ca8b2a1d0c9fe962b05c8a963d21c
usha.kundapur <usha.kundapur@attainia.com>
2018-04-24 16:50:14 -0700

    changing color to theme

--- src/components/common/FieldError.js
+++ src/components/common/FieldError.js
@@ -1,9 +1,10 @@
+import {pathOr} from 'ramda'
 import styled from 'styled-components'
 
 export default styled.div`
     z-index: ${props => props.zIndex || 0};
     width: calc(100% - 1.1em);
-    color: red;
+    color: ${pathOr('crimson', ['theme', 'colors', 'status', 'error'])};;
     position: absolute;
     bottom: -20px;
     font-size: 10px;
