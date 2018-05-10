5e43569f19fe67bbe4f1e6bd8288c30fe853ee12
David Nunez <arizonatribe@gmail.com>
2018-05-10 10:40:25 -0700

    Added Pagination buttons

--- src/components/react-table/index.js
+++ src/components/react-table/index.js
@@ -3,8 +3,14 @@ import TableDropdown from './TableDropdown'
 import TableField from './TableField'
 import TableFieldWithState from './TableFieldWithState'
 import TableFieldWrapperHoC from './TableFieldWrapperHoC'
+import PreviousPageButton from './PreviousPageButton'
+import NextPageButton from './NextPageButton'
+import PagingButtons from './PagingButtons'
 
 export {
+    NextPageButton,
+    PreviousPageButton,
+    PagingButtons,
     ReactTableStyle,
     TableDropdown,
     TableField,
