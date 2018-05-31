2f9fd2f84c9578bacf5a6576128cbe87e5788f7d
David Nunez <arizonatribe@gmail.com>
2018-05-30 17:24:05 -0700

    Added the ability to pass a selector into the middleware, so that it can parse the token from the Redux store, rather than only through storage

--- src/store.js
+++ src/store.js
@@ -5,9 +5,10 @@ import {compose, createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import {createApolloFetch} from 'apollo-fetch'
 
-import {apolloAuthMiddleWare} from './components/auth/middleware'
+import {addTokenToMeta, apolloAuthMiddleWare} from './components/auth/middleware'
 import initialState from './initialState'
 import reducers from './reducers'
+import authDucks from './components/auth/ducks'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL || 'http://localhost/graphql'
@@ -22,6 +23,7 @@ const store = createStore(
     mergeDeepLeft({auth: {baseUrl: process.env.REACT_APP_AUTH_BASE_URL}}, initialState),
     composeEnhancers(
         applyMiddleware(
+            addTokenToMeta({selector: authDucks.selectors.accessToken}),
             apolloAuthMiddleWare(apolloFetch),
             routerMiddleware(browserHistory),
             logger
